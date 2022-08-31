6d6035e1def0a25d7fc6e29c378077076abc4bf7
Filipe <fgallo_dev@hotmail.com>
2022-08-31 09:20:40 -0300

    Adicionando o projeto da Calculadora em console.

---
+++ Calculadora/Calculadora/Operacoes.cs
@@ -0,0 +1,41 @@
+﻿namespace Calculadora
+{
+    public class Operacoes
+    {
+        public Operacoes(double primeiroNumero, double segundoNumero, int escolhaOperacao)
+        {
+            PrimeiroNumero = primeiroNumero;
+            SegundoNumero = segundoNumero;
+            EscolhaOperacao = escolhaOperacao;
+        }
+
+        public double PrimeiroNumero { get; set; }
+        public double SegundoNumero { get; set; }
+        public int EscolhaOperacao { get; set; }
+
+        public double ExibirResultado()
+        {
+            if (EscolhaOperacao == 1)
+            {
+                return Somar();
+            }
+                return 1.0;
+        }
+        public double Somar()
+        {
+            return PrimeiroNumero + SegundoNumero;
+        }
+        public double Substrair()
+        {
+            return PrimeiroNumero + SegundoNumero;
+        }
+        public double Multiplicar()
+        {
+            return PrimeiroNumero * SegundoNumero;
+        }
+        public double Dividir()
+        {
+            return PrimeiroNumero / SegundoNumero;
+        }
+    }
+}
---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,22 @@
+﻿using Calculadora;
+
+int escolhaOperacao;
+double primeiroNumero, segundoNumero;
+
+Console.WriteLine("Seja bem-findo a sua calculadora!\n");
+
+Console.WriteLine("Que operação você deseja fazer?\n 1 - Soma");
+Console.WriteLine("\nEscolha:");
+escolhaOperacao = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("\nDigite o 1º número:");
+primeiroNumero = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("\nDigite o 2º número:");
+segundoNumero = Convert.ToDouble(Console.ReadLine());
+
+Operacoes operacao = new Operacoes(primeiroNumero, segundoNumero, escolhaOperacao);
+
+Console.WriteLine($"\nResultado: {operacao.ExibirResultado()}");
+
+Console.ReadLine();
